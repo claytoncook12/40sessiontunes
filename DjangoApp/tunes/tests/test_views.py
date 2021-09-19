bec37ea138ab7f4df4d0a241297e68d48bddde58
claytoncook12 <claytoncook12@gmail.com>
2021-09-19 13:34:19 -0400

    add tests for list view of tunes

--- DjangoApp/tunes/tests/test_views.py
+++ DjangoApp/tunes/tests/test_views.py
@@ -8,7 +8,6 @@ import pytest
 @pytest.mark.django_db
 class TestDetailView:
     def test_shows_name(self):
-        """ The tunes.detail view shows the tune.name  """
         client = Client()
         
         # Create Tune and Get Id
@@ -18,5 +17,20 @@ class TestDetailView:
         # Get detail view of tune created above
         response = client.get(reverse("tunes:detail", args=(id,)))
 
-        assert response.status_code == 200
-        assert tune.name in response.content.decode()
+        assert response.status_code == 200, "View returns 200 status code"
+        assert tune.name in response.content.decode(), "The tunes.detail view shows the tune.name"
+
+@pytest.mark.django_db
+class TestListView:
+    def test_shows_list(self):
+        client = Client()
+
+        # Create a Few Tune for Database
+        tune1 = TuneFactory()
+        tune2 = TuneFactory(name="Cooley", tune_info="Some info about tune")
+
+        response = client.get(reverse("tunes:list"))
+
+        assert response.status_code == 200, "View return 200 status code"
+        assert tune1.name in response.content.decode(), "tune1.name shows in the list view"
+        assert tune2.name in response.content.decode(), "tune2.name shows in the list view" 
