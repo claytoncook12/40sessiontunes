07fc435ecb2e286fc9d7d17abd1b466fcd4bda89
claytoncook12 <claytoncook12@gmail.com>
2021-09-24 12:21:01 -0400

    add specific return objs check

--- DjangoApp/tunes/tests/test_tunes_combine.py
+++ DjangoApp/tunes/tests/test_tunes_combine.py
@@ -10,7 +10,7 @@ from tunes import tunes_combine
 @pytest.mark.django_db
 class TestMakeSet:
     def test_pull_tunes(self):
-        # Create 4 tunes for database
+        # Create 5 tunes for database
         tune1 = factories.TuneFactory(
             name = "Tune 1",
             tune_type = factories.TuneTypeFactory(
@@ -36,7 +36,7 @@ class TestMakeSet:
             )
         )
         tune5 = factories.TuneFactory(
-            name = "Tune 4",
+            name = "Tune 5",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "jig"
             )
@@ -45,6 +45,17 @@ class TestMakeSet:
         # Check that queryset returns correct objects
         qs = tunes_combine.pull_tunes(num=3,tune_type="reel")
         assert len(qs) == 3, "Should pull only 3 of 5 tunes that are reels"
-
+        assert (tune1 in qs) == True, "Should contain tune1"
+        assert (tune2 in qs) == True, "Should contain tune2"
+        assert (tune3 in qs) == True, "Should contain tune3"
+        assert (tune4 in qs) == False, "Should not contain tune4"
+        assert (tune5 in qs) == False, "Should not contain tune5"
+        
+        # Check that queryset returns correct objects
         qs = tunes_combine.pull_tunes(num=2,tune_type="jig")
         assert len(qs) == 2, "Should pull only 2 of 5 tunes that are jigs"
+        assert (tune1 in qs) == False, "Should not contain tune1"
+        assert (tune2 in qs) == False, "Should not contain tune2"
+        assert (tune3 in qs) == False, "Should not contain tune3"
+        assert (tune4 in qs) == True, "Should contain tune4"
+        assert (tune5 in qs) == True, "Should contain tune5"
--- DjangoApp/tunes/tunes_combine.py
+++ DjangoApp/tunes/tunes_combine.py
@@ -2,10 +2,18 @@
 
 from tunes import models
 
-def pull_tunes(num,tune_type):
+def pull_tunes(num: int, tune_type: str):
     """
-    Queries tunes from database based on number needed and
-    tune_type specified
+    Queries Tunes from database based on number needed
+
+    Parameters
+    ----------
+        num (int): number of tunes to return
+        tune_type (str): type of tune to return
+    
+    Returns
+    -------
+        QuerySet: set that matches parameters
     """
 
     qs = models.Tune.objects.filter(tune_type__tune_type_char=tune_type).order_by('?')[:num]
