c6080dbba0d90595919f875657988a611a2d2904
claytoncook12 <claytoncook12@gmail.com>
2021-09-24 13:08:04 -0400

    move data to setup_data in test class

--- DjangoApp/tunes/tests/test_tunes_combine.py
+++ DjangoApp/tunes/tests/test_tunes_combine.py
@@ -9,53 +9,55 @@ from tunes import tunes_combine
 
 @pytest.mark.django_db
 class TestMakeSet:
-    def test_pull_tunes(self):
+    @pytest.fixture()
+    def setup_data(self):
         # Create 5 tunes for database
-        tune1 = factories.TuneFactory(
+        self.tune1 = factories.TuneFactory(
             name = "Tune 1",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "reel"
             )
         )
-        tune2 = factories.TuneFactory(
+        self.tune2 = factories.TuneFactory(
             name = "Tune 2",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "reel"
             )
         )
-        tune3 = factories.TuneFactory(
+        self.tune3 = factories.TuneFactory(
             name = "Tune 3",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "reel"
             )
         )
-        tune4 = factories.TuneFactory(
+        self.tune4 = factories.TuneFactory(
             name = "Tune 4",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "jig"
             )
         )
-        tune5 = factories.TuneFactory(
+        self.tune5 = factories.TuneFactory(
             name = "Tune 5",
             tune_type = factories.TuneTypeFactory(
                 tune_type_char = "jig"
             )
         )
 
+    def test_pull_tunes(self, setup_data):
         # Check that queryset returns correct objects
         qs = tunes_combine.pull_tunes(num=3,tune_type="reel")
         assert len(qs) == 3, "Should pull only 3 of 5 tunes that are reels"
-        assert (tune1 in qs) == True, "Should contain tune1"
-        assert (tune2 in qs) == True, "Should contain tune2"
-        assert (tune3 in qs) == True, "Should contain tune3"
-        assert (tune4 in qs) == False, "Should not contain tune4"
-        assert (tune5 in qs) == False, "Should not contain tune5"
+        assert (self.tune1 in qs) == True, "Should contain tune1"
+        assert (self.tune2 in qs) == True, "Should contain tune2"
+        assert (self.tune3 in qs) == True, "Should contain tune3"
+        assert (self.tune4 in qs) == False, "Should not contain tune4"
+        assert (self.tune5 in qs) == False, "Should not contain tune5"
         
         # Check that queryset returns correct objects
         qs = tunes_combine.pull_tunes(num=2,tune_type="jig")
         assert len(qs) == 2, "Should pull only 2 of 5 tunes that are jigs"
-        assert (tune1 in qs) == False, "Should not contain tune1"
-        assert (tune2 in qs) == False, "Should not contain tune2"
-        assert (tune3 in qs) == False, "Should not contain tune3"
-        assert (tune4 in qs) == True, "Should contain tune4"
-        assert (tune5 in qs) == True, "Should contain tune5"
+        assert (self.tune1 in qs) == False, "Should not contain tune1"
+        assert (self.tune2 in qs) == False, "Should not contain tune2"
+        assert (self.tune3 in qs) == False, "Should not contain tune3"
+        assert (self.tune4 in qs) == True, "Should contain tune4"
+        assert (self.tune5 in qs) == True, "Should contain tune5"
