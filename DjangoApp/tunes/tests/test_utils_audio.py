efa30b5abcdacad9bd98f5823d99b2f196a7eaca
claytoncook12 <claytoncook12@gmail.com>
2021-09-28 14:27:26 -0400

    add additional tests for utils_audio

--- DjangoApp/tunes/tests/test_utils_audio.py
+++ DjangoApp/tunes/tests/test_utils_audio.py
@@ -1,4 +1,18 @@
-from tunes.utils_audio import mspb
+import pytest
+
+from tunes.utils_audio import mspb, bpm_beats_to_milliseconds
 
 def test_mspb():
     assert mspb(60) == 1000, "Check 60 bpm is 1000 milliseconds"
+
+def test_bpm_beats_to_milliseconds():
+    assert bpm_beats_to_milliseconds(70, 7) == 6000, "Check 70 bpm and 7 beats is 6000 milliseconds"
+    assert bpm_beats_to_milliseconds(90, 3) == 2000, "Check 90 bpm and 3 beats is 2000 milliseconds"
+    assert bpm_beats_to_milliseconds(102, 6) == 3529.41, "Check 102 bpm and 6 beats is 3529.41 milliseconds"
+    assert bpm_beats_to_milliseconds(0, 0) == 0, "Check 0 bpm and 0 beats is 0 milliseconds"
+
+    with pytest.raises(ValueError):
+        bpm_beats_to_milliseconds(-1, 1), "Check that negative bpm raises value error"
+    
+    with pytest.raises(ValueError):
+        bpm_beats_to_milliseconds(1, -1), "Check that negative beats raises value error"
--- DjangoApp/tunes/utils_audio.py
+++ DjangoApp/tunes/utils_audio.py
@@ -9,6 +9,32 @@ def mspb(beats_per_minute: int) -> float:
         float: seconds per beat
     """
 
+    if beats_per_minute < 0:
+        raise ValueError("Beats per minute must be greater than or equal to 0")
+    
+    if beats_per_minute == 0:
+        return 0
+
     bpm = float(beats_per_minute)
 
     return (60/bpm)*1000
+
+def bpm_beats_to_milliseconds(bpm: int, beats: int) -> float:
+    """
+    Calculates milliseconds from beats per minute
+    and number of beats
+
+    Parameters
+    ----------
+        bpm (int): beats per minute
+        beats (int): beats
+    
+    Return
+    ------
+        float: milliseconds of time passed for beats to the hundreth place
+    """
+    
+    if bpm < 0 or beats < 0:
+        raise ValueError("BPM and Beats must both be zero or postive values.")
+
+    return round(mspb(bpm) * beats, 2) 
